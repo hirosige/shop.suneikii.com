74597c3e22d59066852aa68eddbe0d7b7572568a
Hiroshige.Negishi <hirosige1@gmail.com>
2017-12-11 21:14:32 +0700

    add db lib

--- db/seeds/exec.rb
+++ db/seeds/exec.rb
@@ -1,16 +1,6 @@
 require('./db/seeds/entities/concerns/printable.rb')
 
 require('./db/seeds/seed_manager.rb')
-require('./db/seeds/entities/country.rb')
-require('./db/seeds/entities/province.rb')
-require('./db/seeds/entities/district.rb')
-require('./db/seeds/entities/subdistrict.rb')
-require('./db/seeds/entities/subdistrict_v2.rb')
-require('./db/seeds/entities/room_type.rb')
-require('./db/seeds/entities/provider.rb')
-require('./db/seeds/entities/apartment.rb')
-require('./db/seeds/entities/land_category.rb')
-require('./db/seeds/entities/land.rb')
 
 class Exec
   CREATING_PTS = 1000
--- lib/tasks/seed.rake
+++ lib/tasks/seed.rake
@@ -1,10 +1,7 @@
 Dir.glob(File.join(Rails.root, 'db', 'seeds', '*.rb')).each do |file|
   desc "Load the seed data from db/seeds/#{File.basename(file)}."
-  task "db:seed:#{File.basename(file).gsub(/\..+$/, '')}" => :environment do
-    load(file)
-  end
 
-  task "db:seed:all" => :environment do
-    load(file)
+  task "db:seed:exec" => :environment do
+    load(file) if File.basename(file).gsub(/\..+$/, '').eql?('exec')
   end
 end
