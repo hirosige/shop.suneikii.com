e928283261cdc6edf14fc645f63defbd6f7ab366
Hiroshige.Negishi <hirosige1@gmail.com>
2017-12-12 21:47:55 +0700

    updated

--- db/seeds/exec.rb
+++ db/seeds/exec.rb
@@ -6,17 +6,17 @@ class Exec
   CREATING_PTS = 1000
 
   def exec
-    SeedManager.exec(CountrySeeds.new)
-    SeedManager.exec(ProvinceSeeds.new)
-    SeedManager.exec(DistrictSeeds.new)
-    SeedManager.exec(SubDistrictSeeds.new)
-    SeedManager.exec(SubDistrictV2Seeds.new)
-    SeedManager.exec(RoomTypeSeeds.new)
-    SeedManager.exec(ProviderSeeds.new)
-    SeedManager.exec(LandCategorySeeds.new)
+    # SeedManager.exec(CountrySeeds.new)
+    # SeedManager.exec(ProvinceSeeds.new)
+    # SeedManager.exec(DistrictSeeds.new)
+    # SeedManager.exec(SubDistrictSeeds.new)
+    # SeedManager.exec(SubDistrictV2Seeds.new)
+    # SeedManager.exec(RoomTypeSeeds.new)
+    # SeedManager.exec(ProviderSeeds.new)
+    # SeedManager.exec(LandCategorySeeds.new)
 
     # if Rails.env.development?
-      SeedManager.exec(ApartmentSeeds.new(CREATING_PTS))
+    #   SeedManager.exec(ApartmentSeeds.new(CREATING_PTS))
     # end
 
     # land = LandSeeds.new(1000)
